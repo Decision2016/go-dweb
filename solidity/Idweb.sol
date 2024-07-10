002424295017becc00e42aced9c6294b973e5008
Decision <personal@decision01.com>
2024-07-10 15:17:20 +0800

    (evm) feat: solidity simple contract

--- solidity/Idweb.sol
+++ solidity/Idweb.sol
@@ -1,11 +1,51 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-interface IDWeb {
+import "./ownable.sol";
+
+interface IDWeb is Ownable{
     function identity() public view returns (string);
     function boostrap() public view returns (string);
 
     function initial(string identity) public external returns (bool);
     function update(string identity) public external returns (bool);
     function join(string link) public external returns (bool);
+}
+
+contract DWeb is IDWeb{
+    string private _identity;
+    string private _boostrap;
+    bool private inited;
+
+    constructor() {
+        inited = false;
+    }
+
+    function identity() public view returns (string) {
+        return _identity;
+    }
+
+    function boostrap() public view returns (string) {
+        return _boostrap;
+    }
+
+    function initial(string identity) public external returns (bool) {
+        require(!inited, "DWeb: contract has been inited");
+
+        _identity = identity;
+        inited = true;
+        return true;
+    }
+
+    function update(string newIdent) public external returns (bool) {
+        require(inited, "DWeb: contract require to init");
+
+        _identity = newIdent;
+        return true;
+    }
+
+    function join(string link) public external returns (bool) {
+        _boostrap = link;
+        return true;
+    }
 }
