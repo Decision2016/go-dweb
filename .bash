f2e663eddd4341d94a565752d99e5367bc217160
Decision <personal@decision01.com>
2024-08-29 14:53:11 +0800

    (.ignore) feat: ignore tests directory

--- .bash
+++ .bash
@@ -1,3 +1,3 @@
 # 统计代码行数
 
-git log --author="Decision" --since="2024-05-28" --until="2024-07-11" --pretty=tformat: --numstat -- . ":(exclude)static" ":(exclude)example" ":(exclude)go.mod" ":(exclude)plugins/evm/dweb.go" | awk '{ add += $1; subs += $2; loc += $1 - $2 } END { printf "added lines: %s, removed lines: %s, total lines: %s\n", add, subs, loc }'
+git log --author="Decision" --since="2024-05-28" --until="2024-08-29" --pretty=tformat: --numstat -- . ":(exclude)static" ":(exclude)example" ":(exclude)go.mod" ":(exclude)plugins/evm/dweb.go" ":(exclude)plugins/norn/pb" | awk '{ add += $1; subs += $2; loc += $1 - $2 } END { printf "added lines: %s, removed lines: %s, total lines: %s\n", add, subs, loc }'
